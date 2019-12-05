5a4f319894a713e30d813e267ff329f8726566f8
somewhatlurker <52014015+somewhatlurker@users.noreply.github.com>
2019-12-05 16:17:12 +1100

    add proximity to mpr121example

--- src/mpr121example/mpr121example.ino
+++ src/mpr121example/mpr121example.ino
@@ -1,5 +1,6 @@
 #define NUM_MPRS 3 // must be using sequential addresses
 #define ANALOG_OUTPUT false
+#define PROXIMITY_ENABLE false
 
 #include "mpr121.h"
 
@@ -13,6 +14,7 @@ void setup() {
     mpr = mpr121(0x5a + i);
     mpr.ESI = MPR_ESI_1; // get 4ms response time (4 samples * 1ms rate)
     mpr.autoConfigUSL = 256 * (3200 - 700) / 3200; // set autoconfig for 3.2V
+    mpr.proxEnable = PROXIMITY_ENABLE ? MPR_ELEPROX_0_TO_11 : MPR_ELEPROX_DISABLED;
     mpr.startMPR(12);
   }
 
@@ -20,17 +22,19 @@ void setup() {
 }
 
 void loop() {
+  int numElectrodes = PROXIMITY_ENABLE ? 13 : 12;
+  
   for (int i = 0; i < NUM_MPRS; i++) {
     mpr121 &mpr = mprs[i];
     if (ANALOG_OUTPUT) {
-      short* values = mpr.readElectrodeData(0, 12); // read 12 electrodes, starting from electrode 0
-      for (int j = 0; j < 12; j++)
+      short* values = mpr.readElectrodeData(0, numElectrodes); // read all electrodes, starting from electrode 0
+      for (int j = 0; j < numElectrodes; j++)
         Serial.print(values[j]);
         Serial.print(" ");
     }
     else {
       bool* touches = mpr.readTouchState();
-      for (int j = 0; j < 12; j++)
+      for (int j = 0; j < numElectrodes; j++)
         Serial.print(touches[j]);
     }
     Serial.print(" ");
